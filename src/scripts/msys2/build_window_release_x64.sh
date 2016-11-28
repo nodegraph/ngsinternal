5501b7368727ad85da143a7d44dd14caded2b8a7
Smash Browse <smashbrowse@users.noreply.github.com>
2016-11-28 11:15:03 -0800

    - build script for windows release x64

---
+++ src/scripts/msys2/build_window_release_x64.sh
@@ -0,0 +1,18 @@
+# Jenkins Pipeline Definition.
+#node {
+#    bat "D:\\src\\ngsinternal\\src\\scripts\\msys2\\launch_msys2.bat ARCH_WINDOWS x64 D:\\src\\ngsinternal\\src\\scripts\\msys2\\build_window_release_x64.sh"
+#}
+
+
+echo "WINDOWS RELEASE X64";
+export ARCH=ARCH_WINDOWS;
+export ARCH_BITS=x64;
+source /d/src/ngsinternal/src/scripts/msys2/setup_env.sh;
+rm -fr /d/wr64;
+mkdir /d/wr64;
+cd /d/wr64;
+git clone git@gitlab.com:nodegraph/ngsinternal.git;
+mkdir build;
+cd build;
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ../ngsinternal/src;
+ninja install;
