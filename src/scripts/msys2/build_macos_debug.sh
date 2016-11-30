f44cf54c449d5c9bbb89c14c869d35114467f847
nodegraph <shingo.takagi@gmail.com>
2016-11-30 13:13:50 -0800

    no message

--- src/scripts/msys2/build_macos_debug.sh
+++ src/scripts/msys2/build_macos_debug.sh
@@ -6,6 +6,7 @@
 #}
 
 echo "MACOS DEBUG"
+echo `whoami`
 export ARCH=ARCH_MACOS
 export ARCH_BITS=x64
 source /Users/raindrop/src/ngsinternal/src/scripts/msys2/setup_env.sh
@@ -13,10 +14,10 @@ cd /Users/raindrop/dev/macos
 rm -fr macos_debug_jenkins
 mkdir macos_debug_jenkins
 cd macos_debug_jenkins
-git clone git@gitlab.com:nodegraph/ngsinternal.git;
-mkdir build;
-cd build;
-cmake -G "Eclipse CDT4 - Unix Makefiles" -DARCH=ARCH_MACOS -DCMAKE_BUILD_TYPE=Debug ../ngsinternal/src
+git clone git@gitlab.com:nodegraph/ngsinternal.git
+mkdir build
+cd build
+cmake -G "Eclipse CDT4 - Ninja" -DARCH=ARCH_MACOS -DCMAKE_BUILD_TYPE=Debug ../ngsinternal/src
 make install -j8
 
 
