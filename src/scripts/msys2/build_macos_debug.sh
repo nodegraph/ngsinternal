a8768a768c3de02d87e6b69c1cabab531f063c43
Smash Browse <smashbrowse@users.noreply.github.com>
2016-11-30 10:36:29 -0800

    - macos build scripts

---
+++ src/scripts/msys2/build_macos_debug.sh
@@ -0,0 +1,20 @@
+# Jenkins Pipeline Definition.
+#node {
+#    sh "source /Users/raindrop/src/ngsinternal/src/scripts/msys2/build_macos_debug.sh"
+#}
+
+echo "MACOS DEBUG"
+export ARCH=ARCH_MACOS
+export ARCH_BITS=x64
+source /Users/raindrop/src/ngsinternal/src/scripts/msys2/setup_env.sh
+cd /Users/raindrop/dev/macos
+rm -fr macos_debug_jenkins
+mkdir macos_debug_jenkins
+cd macos_debug_jenkins
+git clone git@gitlab.com:nodegraph/ngsinternal.git;
+mkdir build;
+cd build;
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DARCH=ARCH_MACOS -DCMAKE_BUILD_TYPE=Debug ../ngsinternal/src
+make install -j8
+
+
---
+++ src/scripts/msys2/build_macos_release.sh
@@ -0,0 +1,20 @@
+# Jenkins Pipeline Definition.
+#node {
+#    sh "source /Users/raindrop/src/ngsinternal/src/scripts/msys2/build_macos_release.sh"
+#}
+
+echo "MACOS RELEASE"
+export ARCH=ARCH_MACOS
+export ARCH_BITS=x64
+source /Users/raindrop/src/ngsinternal/src/scripts/msys2/setup_env.sh
+cd /Users/raindrop/dev/macos
+rm -fr macos_release_jenkins
+mkdir macos_release_jenkins
+cd macos_release_jenkins
+git clone git@gitlab.com:nodegraph/ngsinternal.git;
+mkdir build;
+cd build;
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DARCH=ARCH_MACOS -DCMAKE_BUILD_TYPE=Release ../ngsinternal/src
+make install -j8
+
+
