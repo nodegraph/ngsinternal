1437083767d0c05f408c6679a983fb3e45ddb163
Smash Browse <smashbrowse@users.noreply.github.com>
2016-11-30 11:13:54 -0800

    no message

--- src/scripts/msys2/build_window_release_x64.sh
+++ src/scripts/msys2/build_window_release_x64.sh
@@ -5,7 +5,7 @@
 #    bat "D:\\src\\ngsinternal\\src\\scripts\\msys2\\launch_msys2.bat ARCH_WINDOWS x64 D:\\src\\ngsinternal\\src\\scripts\\msys2\\build_window_release_x64.sh"
 #}
 
-echo "WINDOWS RELEASE X64";
+echo "WINDOWS RELEASE";
 export ARCH=ARCH_WINDOWS;
 export ARCH_BITS=x64;
 source /d/src/ngsinternal/src/scripts/msys2/setup_env.sh;
