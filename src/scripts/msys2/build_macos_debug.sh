85d0eda8e36dce56b3474ad28670e88eecbbd920
Smash Browse <smashbrowse@users.noreply.github.com>
2016-11-30 14:17:36 -0800

    no message

--- src/scripts/msys2/build_macos_debug.sh
+++ src/scripts/msys2/build_macos_debug.sh
@@ -6,18 +6,17 @@
 #}
 
 echo "MACOS DEBUG"
-echo `whoami`
-export ARCH=ARCH_MACOS
-export ARCH_BITS=x64
-source /Users/raindrop/src/ngsinternal/src/scripts/msys2/setup_env.sh
+#export ARCH=ARCH_MACOS
+#export ARCH_BITS=x64
+cd /Users/raindrop/src/ngsinternal
+git pull
 cd /Users/raindrop/dev/macos
 rm -fr macos_debug_jenkins
 mkdir macos_debug_jenkins
 cd macos_debug_jenkins
-git clone git@gitlab.com:nodegraph/ngsinternal.git
 mkdir build
 cd build
-cmake -G "Eclipse CDT4 - Ninja" -DARCH=ARCH_MACOS -DCMAKE_BUILD_TYPE=Debug ../ngsinternal/src
-make install -j8
+cmake -G "Eclipse CDT4 - Ninja" -DARCH=ARCH_MACOS -DCMAKE_BUILD_TYPE=Debug ~/src/ngsinternal/src
+ninja install
 
 
--- src/scripts/msys2/build_macos_release.sh
+++ src/scripts/msys2/build_macos_release.sh
@@ -6,17 +6,17 @@
 #}
 
 echo "MACOS RELEASE"
-export ARCH=ARCH_MACOS
-export ARCH_BITS=x64
-source /Users/raindrop/src/ngsinternal/src/scripts/msys2/setup_env.sh
+#export ARCH=ARCH_MACOS
+#export ARCH_BITS=x64
+cd /Users/raindrop/src/ngsinternal
+git pull
 cd /Users/raindrop/dev/macos
 rm -fr macos_release_jenkins
 mkdir macos_release_jenkins
 cd macos_release_jenkins
-git clone git@gitlab.com:nodegraph/ngsinternal.git;
-mkdir build;
-cd build;
+mkdir build
+cd build
 cmake -G "Eclipse CDT4 - Ninja" -DARCH=ARCH_MACOS -DCMAKE_BUILD_TYPE=Release ../ngsinternal/src
-make install -j8
+ninja install
 
 
