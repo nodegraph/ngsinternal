16f844783eb7bb88dc11ccc2ad6322282ddf05af
Smash Browse <smashbrowse@gmail.com>
2017-01-10 10:31:09 -0800

    -removing some unused files

--- src/components/computes/variantutils.h
+++
@@ -1,9 +0,0 @@
-
-
-//  static bool variant_is_bool(const QVariant& value);
-//  static bool variant_is_list(const QVariant& value);
-//  static bool variant_is_map(const QVariant& value);
-//
-//  static void merge_maps(QVariantMap& target, const QVariantMap& giver);
-//  static void merge(QVariant& target, const QVariant& source);
-//  static QVariant deep_merge(const QVariant& target, const QVariant& source);
