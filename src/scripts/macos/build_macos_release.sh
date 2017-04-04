ef2fb545add1b1d6bf9db2880b599e5d53ec8e3c
Smash Browse <smashbrowse@gmail.com>
2017-04-04 13:36:34 -0700

    testing

--- src/scripts/macos/build_macos_release.sh
+++ src/scripts/macos/build_macos_release.sh
@@ -32,7 +32,16 @@ ninja install
 ninja fill_robodownloader
 
 # Get permission to use codesign certificates.
-security unlock-keychain -p $1 "/Users/raindrop/Library/Keychains/login.keychain-db"
+#security unlock-keychain -p $1 "/Users/raindrop/Library/Keychains/login.keychain-db"
+
+keychain=$HOME/Library/Keychains/login.keychain
+security unlock-keychain -p $1 ${keychain} &>/dev/null
+   
+if [ $? -ne 0 ];then
+	echo "Cannot open keychain ${keychain}"
+	exit 1
+fi
+   
 
 #security set-key-partition-list -S apple: -k $1 -D raindrop -t private
 #security set-key-partition-list -s -k $1 -D raindrop -t private
