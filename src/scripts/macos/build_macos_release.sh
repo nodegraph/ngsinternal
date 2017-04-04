baeafb026f82b77f5f18fcc9bcf1b164c41e40c5
Smash Browse <smashbrowse@gmail.com>
2017-04-04 14:31:27 -0700

    no message

--- src/scripts/macos/build_macos_release.sh
+++ src/scripts/macos/build_macos_release.sh
@@ -5,14 +5,19 @@
 #      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'adf8e49d-3391-4d48-947b-2b8d6fcc1170',
 #                            usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
 #                //available as an env variable, but will be masked if you try to print it out any which way
-				 echo ${PASSWORD} | sed -e 's/\(.\)/\1 /g'
-				 echo ${USERNAME} | sed -e 's/\(.\)/\1 /g'
-				 security unlock-keychain -p $PASSWORD $HOME/Library/Keychains/login.keychain-db
 #                sh "source /Users/raindrop/src/ngsinternal/src/scripts/macos/build_macos_release.sh"
 #            }
 #}
 
 whoami
+
+echo ${PASSWORD} | sed -e 's/\(.\)/\1 /g'
+echo ${USERNAME} | sed -e 's/\(.\)/\1 /g'
+
+echo ${env.PASSWORD} | sed -e 's/\(.\)/\1 /g'
+echo ${env.USERNAME} | sed -e 's/\(.\)/\1 /g'
+
+security unlock-keychain -p $PASSWORD $HOME/Library/Keychains/login.keychain-db
 
 # Get permission to use codesign certificates.
 #security unlock-keychain -p $1 "/Users/raindrop/Library/Keychains/login.keychain-db"
